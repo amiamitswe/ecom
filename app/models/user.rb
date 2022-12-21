ca2b7eddaff72de89e7fccf5cc61a43f84f702f3
Amit Samadder <amiamitswe@gmail.com>
2022-12-21 19:10:10 +0600

    update user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,16 @@
 class User < ApplicationRecord
-  has_secure_password
-  # before_save :default_values
+  before_save { self.email = email.downcase }
+  has_many :categories, dependent: :destroy
 
   validates :role, inclusion: { in: %w[admin user agent] }
+  validates :username, presence: true
+  validates_length_of :username, maximum: 25, minimum: 3
 
-  # private
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 105 },
+            format: { with: VALID_EMAIL_REGEX }
 
-  # def set_defaults
-  #   role = "user" if role.blank?
-  # end
-
-
-  # def default_values
-  #   self.role = 'user' # note self.status = 'P' if self.status.nil? might better for boolean fields (per @frontendbeauty)
-  # end
+  has_secure_password
 end
